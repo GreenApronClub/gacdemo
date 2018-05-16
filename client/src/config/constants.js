20ee07685e854dedaef4401ee7cf029979272daf
Foojinga <calvinmwariama@gmail.com>
2018-05-16 19:49:09 -0400

    Working AWS S3 Upload V1

--- client/src/config/constants.js
+++ client/src/config/constants.js
@@ -1,6 +1,6 @@
 const PROD_URL = "https://gacdemo.herokuapp.com";
 const DEV_URL = "http://localhost:8080";
 
-export const ROOT_URL = process.env.REACT_APP_ENV === 'production'
+export const ROOT_URL = process.env.REACT_APP_ENV === 'prod'
   ? PROD_URL
   : DEV_URL;
